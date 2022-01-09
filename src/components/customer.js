fef0bd65deb9e596ad960ec0cfe28cea6d3a36b9
Retajalotaibi <63907341+Retajalotaibi@users.noreply.github.com>
2022-01-09 03:17:59 +0300

    everything is added

--- src/components/customer.js
+++ src/components/customer.js
@@ -5,15 +5,15 @@ const Customer = () => {
     <div className="cus-container">
       <div className="cus-info">
         <img src={avatar} />
-        <h3>Jouri Alfuraih</h3>
+        <h3>Nour</h3>
       </div>
       <p>
-        Hello I'm Ghadder, I went to Kuwait for the first time in 2021 and I
-        chose Grrovly Travels to be my guaid in my trip, My experience was
-        amazing I got to go to super interesting places, I really liked the up
-        saide down house it had a really nice Kuwaiti touch, And the stuff were
-        absolutely amazing, if you are planning to visit Kuwait you should go
-        with groovy travles !
+        Hello I'm Nour, I went to Kuwait for the first time in 2021 and I chose
+        Grrovly Travels to be my guaid in my trip, My experience was amazing I
+        got to go to super interesting places, I really liked the up saide down
+        house it had a really nice Kuwaiti touch, And the stuff were absolutely
+        amazing, if you are planning to visit Kuwait you should go with groovy
+        travles !
       </p>
     </div>
   );
--- src/components/founders.js
+++ src/components/founders.js
@@ -1,7 +1,36 @@
 import React from "react";
-
+import f1 from "../assets/F-1.png";
+import f2 from "../assets/F-2.png";
+import f3 from "../assets/F-3.png";
+import "../styles/founder.css";
 const Founders = () => {
-  return <div></div>;
+  return (
+    <div className="container-FS">
+      <h2>founder and their story</h2>
+      <div className="FS">
+        <div className="text-p">
+          <p>
+            Groovy travels in more than just a travel company, we seek to help
+            everyone to see how many amazing places there is in kuwait{" "}
+          </p>
+        </div>
+        <div className="images">
+          <div>
+            <img src={f1} />
+            <p>Retaj</p>
+          </div>
+          <div>
+            <img src={f2} />
+            <p>Ghezlan</p>
+          </div>{" "}
+          <div>
+            <img src={f3} />
+            <p>Jouri</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Founders;
